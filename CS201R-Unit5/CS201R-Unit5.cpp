586807d7e8870759d6bc470fc649314136964502
Joan Gladbach <jjgr29@umsystem.edu>
2024-09-04 13:19:17 -0500

    updates to main

--- CS201R-Unit5/CS201R-Unit5.cpp
+++ CS201R-Unit5/CS201R-Unit5.cpp
@@ -5,14 +5,14 @@
 
 int main() {
     //EXAMPLES Demonstrating file IO
-    example1();
-    example2();
-    example3();
-    example4();
-    example5();
-    example6();
-    example7();
-    example8();
-    example9();
-    example10();
+    example1();   //File I/O
+    example2();   //File I/O, structs, vector of structs
+    example3();   //File I/O & CSV file input using string stream & output formatting
+    example4();   //File I / O & CSV file input using string stream& output formatting
+    example5();   //File I/O & CSV file input using string stream & vector manipulation
+    example6();   //error handling using isDigits
+    example7();   //error handling using try/throw/catch
+    example8();   //error handling using try/throw/catch
+    example9();   //error handling using fail bits
+    example10();  //try/catch with functions
 }
